ac948675d23ef47fcacc1dd06e0a801623d84d6a
arpanpathak <arpanpathak1996@gmail.com>
2016-11-19 20:03:47 +0530

    Added

---
+++ Algorithms/String Matching Algorithms/C++ Implementations/kmp.cpp
@@ -0,0 +1,36 @@
+/** Knuth Morris Pattern Matching, Time Complexity : O(m+n)
+ ** @author: Arpan Pathak **/
+#include <bits/stdc++.h>
+using namespace std;
+void suffix_prefix(vector<int> &spa,string pattern){
+
+    for(int i=0,j=1;j<pattern.length();){
+        if(pattern.at(i)==pattern.at(j)) spa[j++]=++i;
+        else if(i!=0) i=spa[i-1];
+        else          j++;
+    }
+}
+void kmp(vector<int> &spa,string str,string pattern){
+    bool c=false;
+    for(int i=0,j=0;i<str.length() && j<str.length();)
+    {
+        if(str.at(i)==pattern.at(j))
+        {
+            if(j==pattern.length()-1)
+            {
+                 cout<<"Pattern found at "<<i-j<<endl;
+                 c=true; j=0; i++;
+            }
+             else{  i++; j++; }
+        }
+        else if(j!=0) j=spa[j-1];
+        else          i++;
+    }
+    if(!c) cout<<"Not Found"<<endl;
+}
+int main(){
+    string str="abcddddabcfffddabcafafkfknabc",pattern="abcfx";
+    vector<int> spa(str.length());
+    kmp(spa,str,pattern);
+    return 0;
+}
